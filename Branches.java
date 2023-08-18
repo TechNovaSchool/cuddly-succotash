7dcc99098c4ba5d3eb9dcfdb08b2814ece0f9ace
andrewnova <tech.easy1@outlook.com>
2023-08-17 19:50:12 -0500

    second PR

--- Branches.java
+++ Branches.java
@@ -1,5 +1,6 @@
 public class Branches {
     public static void main(String[] args) {
         System.out.println("This is my first branch, say hello");
+        System.out.println("Second PR");
     }
 }
